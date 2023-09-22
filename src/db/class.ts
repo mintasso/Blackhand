9c2cf5402b47b62842a7276720ee392fa2eac1bf
Sebastian <124442838+VimtageCS@users.noreply.github.com>
2023-09-22 21:18:15 +0200

    something

--- src/db/class.ts
+++ src/db/class.ts
@@ -1,44 +1,42 @@
+import { Client, GatewayIntentBits } from 'discord.js'; // Import the necessary Discord.js modules
 import { MongoClient } from 'mongodb'
 
-// interface UserBanInfo {
-//     nickname: string;
-//     reason_of_ban: string;
-// }
+interface UserInfo {
+  nickname: string;
+  id: string;
+  reason: string;
+  proof: string;
+  status: number;
+}
 
 export class DB {
     constructor() {
 
     }
 
-     async CheckIfUserBlacklisted(givenId: number): Promise<boolean> {
-        const uri = process.env.mongodburi;
+    async CheckIfUserBlacklisted(givenId: number): Promise<UserInfo | null> {
+        const uri = process.env.mongodb_uri;
         const client = new MongoClient(uri);
       
-        try {
           await client.connect();
+    
+          const database = client.db('ExterminatorDB');
+          const collection = database.collection('Blacklist');
       
-          const database = client.db('Blacklist'); // Change this to your database name
-          const collection = database.collection('Blacklist'); // Change this to your collection name
-      
-          // Check if the integer exists in the collection
-          const result = await collection.findOne({ userid:givenId });
-      
-          return !!result; // Returns true if the integer is found, false otherwise
-        } finally {
-          client.close();
-        }
-      }
-      Example(givenID: number) {
-        this.CheckIfUserBlacklisted(givenID)
-        .then((exists) => {
-            if (exists) {
-                console.log("User is blacklisted.")
-            }
-            else {
-                console.log("User is not blacklisted.")
-            }
-        }
-        )
+          const result = await collection.findOne({ userid: givenId }); // null - none, status 1 - listed, status 2 - banned
+		if (result) {
+			const userInfo: UserInfo = {
+				nickname: result.nickname,
+				id: result.id,
+				reason: result.reason,
+        proof: result.proof,
+				status: result.status,
+			  };
+			  return userInfo;
+		}
+		else {
+			return null;
+		}
 
   }
       
@@ -57,6 +55,47 @@ export class DB {
     //         reason_of_ban: "",
     //     }
     // }
-    
-
 }
+
+
+
+
+// Assuming your bot client is instantiated elsewhere in your code
+const botClient = new Client({
+  intents: [
+    GatewayIntentBits.Guilds, // Enable guild (server) related intents
+    GatewayIntentBits.GuildMembers, // Enable guild member intents
+  ],
+});
+
+// Define the CheckAllUsers function
+async function CheckAllUsers(guildId) {
+  try {
+    const guild = await botClient.guilds.fetch(guildId); // Fetch the guild by its ID
+
+    if (!guild) {
+      console.error('Guild not found.');
+      return;
+    }
+
+    const members = await guild.members.fetch(); // Fetch all members in the guild
+
+    for (const [memberId, member] of members) {
+      // Call the CheckIfUserBlacklisted function for each member's ID
+      const result = await this.CheckIfUserBlacklisted(member.id);
+      
+      if (result) {
+        // Handle the result as needed
+        console.log(`User ${member.user.tag} is blacklisted with reason: ${result.reason}`);
+      } else {
+        console.log(`User ${member.user.tag} is not blacklisted.`);
+      }
+    }
+  } catch (error) {
+    console.error('Error in CheckAllUsers:', error);
+  }
+}
+
+// Example usage: assuming you have a guildId from your command
+const guildId = '';
+CheckAllUsers(guildId);
--- src/db/index.ts
+++ src/db/index.ts
@@ -2,6 +2,5 @@
 import { DB } from './class.js';
 
 const db = new DB();
-const givenId = 420;
 
-db.Example()
+
