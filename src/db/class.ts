2757959868c59add10e15ac9aedf9e97e764c582
Sebastian <124442838+VimtageCS@users.noreply.github.com>
2023-09-23 13:41:45 +0200

    yo

--- src/db/class.ts
+++ src/db/class.ts
@@ -1,9 +1,8 @@
-import { Client, GatewayIntentBits } from 'discord.js'; // Import the necessary Discord.js modules
-import { MongoClient } from 'mongodb'
-
+import { MongoClient } from 'mongodb';
+import { botClient } from './index';
+require('dotenv').config()
 interface UserInfo {
-  nickname: string;
-  id: string;
+  userid: string;
   reason: string;
   proof: string;
   status: number;
@@ -13,11 +12,11 @@ export class DB {
     constructor() {
 
     }
-
-    async CheckIfUserBlacklisted(givenId: number): Promise<UserInfo | null> {
-        const uri = process.env.mongodb_uri;
-        const client = new MongoClient(uri);
+    async CheckIfUserBlacklisted(givenId: string): Promise<UserInfo | null> {
+        
       
+      const uri = (process.env.mongo);
+      const client = new MongoClient("mongodb+srv://DiscordBot1:T2j0iKvXuhyijT5U@cluster0.fwwvpop.mongodb.net/?retryWrites=true&w=majority");
           await client.connect();
     
           const database = client.db('ExterminatorDB');
@@ -26,12 +25,12 @@ export class DB {
           const result = await collection.findOne({ userid: givenId }); // null - none, status 1 - listed, status 2 - banned
 		if (result) {
 			const userInfo: UserInfo = {
-				nickname: result.nickname,
-				id: result.id,
+				userid: result.userid,
 				reason: result.reason,
         proof: result.proof,
 				status: result.status,
-			  };
+			  }
+        console.log(userInfo)
 			  return userInfo;
 		}
 		else {
@@ -39,63 +38,22 @@ export class DB {
 		}
 
   }
-      
-    // add_user(id: number, nick: string, reason_of_ban: string) {
-
-    // }
-    // delete_user_by_id(id: number) {
-
-    // }
-    // delete_user_by_nickname(id: number) {
-
-    // }
-    // get_black_list(): UserBanInfo { // prints all nicknames of banned peoples and reason
-    //     return {
-    //         nickname: "",
-    //         reason_of_ban: "",
-    //     }
-    // }
-}
-
-
-
-
-// Assuming your bot client is instantiated elsewhere in your code
-const botClient = new Client({
-  intents: [
-    GatewayIntentBits.Guilds, // Enable guild (server) related intents
-    GatewayIntentBits.GuildMembers, // Enable guild member intents
-  ],
-});
-
-// Define the CheckAllUsers function
-async function CheckAllUsers(guildId) {
-  try {
-    const guild = await botClient.guilds.fetch(guildId); // Fetch the guild by its ID
-
+  async CheckAllUsers(guildId: string) {
+    const guild = await botClient.guilds.fetch(guildId)
+    console.log(guild)
     if (!guild) {
-      console.error('Guild not found.');
+      console.log("Guild not found.")
       return;
     }
+    const members = await guild.members.fetch()
+    console.log(members)
+    members.forEach((member) => {
+      const userId = member.user.id;
+      this.CheckIfUserBlacklisted(userId);
+    });
+    
 
-    const members = await guild.members.fetch(); // Fetch all members in the guild
 
-    for (const [memberId, member] of members) {
-      // Call the CheckIfUserBlacklisted function for each member's ID
-      const result = await this.CheckIfUserBlacklisted(member.id);
-      
-      if (result) {
-        // Handle the result as needed
-        console.log(`User ${member.user.tag} is blacklisted with reason: ${result.reason}`);
-      } else {
-        console.log(`User ${member.user.tag} is not blacklisted.`);
-      }
-    }
-  } catch (error) {
-    console.error('Error in CheckAllUsers:', error);
   }
 }
 
-// Example usage: assuming you have a guildId from your command
-const guildId = '';
-CheckAllUsers(guildId);
--- src/db/index.ts
+++ src/db/index.ts
@@ -1,5 +1,31 @@
-
-import { DB } from './class.js';
-
+require('dotenv').config()
+import { Client,  IntentsBitField, Events } from "discord.js";
+import { DB } from './class'
 const db = new DB();
+
+const guildId = '1154857944744210484';
+
+
+let intents = [
+    IntentsBitField.Flags.Guilds, 
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.GuildIntegrations,
+    IntentsBitField.Flags.GuildMembers,
+    IntentsBitField.Flags.GuildModeration,
+]
+
+export const botClient = new Client({intents: intents })
+
+botClient.on('shardReady', async (shardId) => {
+    console.log(`Shard ${shardId} is ready`);
+   db.CheckAllUsers(guildId)
+    
+})
+
+botClient.login(process.env.DISCORD_TOKEN)
+
+
+
+
+
 
