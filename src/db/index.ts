be09aac33edc40d7d31736efdfec629dc1579fef
Sebastian <124442838+VimtageCS@users.noreply.github.com>
2023-10-04 15:59:54 +0200

    format

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,9 +1,12 @@
-import { DB } from './class.js';
+import { DB } from "./class.js";
 
 const db = new DB();
-const givenId = ["1154858064722272277", "864605650020597770", "1154858064722272277", "1154859541503152318"]
-const guild_id = "1154857944744210484"
-db.CheckIfUserBlacklisted(givenId)
-db.get_server_settings(guild_id)
-
-
+const givenId = [
+  "1154858064722272277",
+  "864605650020597770",
+  "1154858064722272277",
+  "1154859541503152318",
+];
+const guild_id = "1154857944744210484";
+db.CheckIfUserBlacklisted(givenId);
+db.get_server_settings(guild_id);
